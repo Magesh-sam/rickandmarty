b28266ba3982c84019327d5018878f3dbb03824c
magesh-sam <mageshkannanam@gmail.com>
2023-11-18 18:44:43 +0530

    basic setup for rick  & morty with react query

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <main>
-      <h1>Rick and Morty</h1>
+    <main className="max-w-screen h-screen flex justify-center">
+      <img
+        src="../../rickandmorty.png"
+        alt="Rick and Morty Title"
+        className="h-20"
+      />
     </main>
   );
 }
